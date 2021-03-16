03ef5f4ad2f1312def1ac7369761428b7f180d76
shannon <s.francis@se19.qmul.ac.uk>
2021-03-16 17:40:50 +0000

    Update FDMEmployee.java

--- src/main/FDMEmployee.java
+++ src/main/FDMEmployee.java
@@ -1,5 +1,9 @@
 package main;
 import java.util.*;
+import java.time.*; 
+
+
+import jdk.jfr.Enabled;
 
 public class FDMEmployee extends User {
 
@@ -8,49 +12,45 @@ public class FDMEmployee extends User {
 	Health health;
 	Collection<AmbientSounds> ambientSoundExercises;
 	Collection<WorkoutExcerciseAttempt> workoutExcerciseAttempts;
-	private int age;
+	LocalDateTime dateOfBirth;
 
 	/**
 	 * 
-	 * @param nname
-	 * @param password
-	 * @param userName
-	 * @param email
-	 * @param aage
-	 * @param height
-	 * @param weight
 	 */
-	public boolean FDMEmployee(String nname, String password, String userName, String email, int aage, int height, int weight) {
-		// TODO - implement FDMEmployee.FDMEmployee
-		throw new UnsupportedOperationException();
+	public FDMEmployee(String nname, String password, String userName, String email, LocalDateTime ddate, int height, int weight) {
+		super(nname, password, userName, email);
+		dateOfBirth = ddate;
+		health = new Health(height, weight);
 	}
 
 	/**
 	 * 
-	 * @param mentalHealthAmbassador
-	 * @param booking
+	 * @param mentalHealthAmbassador the mental health ambassador that the appointment will be with
+	 * @param booking the timeslot that contains the time that the appointment will be at
 	 */
 	public void bookAppointment(MentalHealthAmbassador mentalHealthAmbassador, TimeSlot booking) {
-		// TODO - implement FDMEmployee.bookAppointment
-		throw new UnsupportedOperationException();
+		Appointment app = new Appointment(mentalHealthAmbassador, this, booking.getDateandTime());
+		appointments.add(app);
+
+
 	}
 
 	/**
-	 * 
-	 * @param appointment
+	 * cancels an appointment
+	 * @param appointment the appointment to be cancelled.
 	 */
 	public void cancelAppointment(Appointment appointment) {
-		// TODO - implement FDMEmployee.cancelAppointment
-		throw new UnsupportedOperationException();
+		appointments.remove(appointment);
+		Inventory.getInstance().getListOfUsers().remove(appointment);
 	}
 
 	/**
 	 * 
-	 * @param appointment
+	 * @param appointment the appoiintment to be scheduled.
+	 * @param newbooking the timeslot that contains the time that the appointment will now be at
 	 */
-	public void rescheduleAppointment(Appointment appointment) {
-		// TODO - implement FDMEmployee.rescheduleAppointment
-		throw new UnsupportedOperationException();
+	public void rescheduleAppointment(Appointment appointment, TimeSlot newbooking) {
+		appointment.setTimeslot(newbooking); // add to appointment / timeslot????
 	}
 
 	/**
@@ -58,49 +58,86 @@ public class FDMEmployee extends User {
 	 * @param availableMindfulnessExercise
 	 */
 	public void attemptMindfulnessExercise(MindfulnessExercise availableMindfulnessExercise) {
-		// TODO - implement FDMEmployee.attemptMindfulnessExercise
-		throw new UnsupportedOperationException();
+		MindfulnessExerciseAttempt attempt = new MindfulnessExcerciseAttempt(availableMindfulnessExercise);
+		mindfulnessExerciseAttempts.add(attempt);
 	}
 
 	/**
 	 * 
-	 * @param availableWorkoutExercise
+	 * @param availableWorkoutExercise the excercise the user is attempted / to be added as a reference in the ne wobject
 	 */
 	public void attemptWorkoutExercise(WorkoutExercise availableWorkoutExercise) {
-		// TODO - implement FDMEmployee.attemptWorkoutExercise
-		throw new UnsupportedOperationException();
-	}
-
-	public String viewStatistics() {
-		// TODO - implement FDMEmployee.viewStatistics
-		throw new UnsupportedOperationException();
+		WorkoutExerciseAttempt attempt = new WorkoutExcerciseAttempt(availableWorkoutExercise);
+		mindfulnessExerciseAttempts.add(attempt);
 	}
 
 	/**
 	 * 
+	 * @param availableWorkoutExercise the excercise the user is attempted / to be added as a reference in the ne wobject
+	 */
+	public void attemptStep(WorkoutExercise availableWorkoutExercise) {
+		WorkoutExerciseAttempt attempt = new WorkoutExcerciseAttempt(availableWorkoutExercise);
+		mindfulnessExerciseAttempts.add(attempt);
+	}
+
+
+	/**
+	 * 
+	 * returns a set of statistics for the employee
+	 */
+	public String viewStatistics() {
+		String stats = "";
+		throw new UnsupportedOperationException();
+		stats += ToString(health.getBMI());
+		stats+= " - current BMI \n";
+		stats+= toString(health.getCurrentHeight());
+		stats+= " - current height \n";
+		stats+= toString(health.getCurrentWeight());
+		stats+= " - current weight \n";
+
+
+		//looping attempt ist....
+		//targets acheived 
+		//how am I to get the targets as theres no list....
+		//add list of targers - 
+		//getter for list and setter - add another 
+		return stats;
+	}
+
+	/**
+	 * deletes the excercise attempt
 	 * @param exerciseAttempt
 	 */
 	public boolean deleteExerciseAttempt(ExerciseAttempt exerciseAttempt) {
-		// TODO - implement FDMEmployee.deleteExerciseAttempt
-		throw new UnsupportedOperationException();
+		workoutExcerciseAttempts.remove(exerciseAttempt);
+		mindfulnessExerciseAttempts.remove(exerciseAttempt);
+
 	}
 
 	/**
 	 * 
-	 * @param mindfulnessExerciseAttempt
+	 * @param ExerciseAttempt the activity to be edited
+	 * @param newDuration the new time the activity actually took 
+	 * @param newSteps if the attempt is a step excercise, this parameter represents the new amount of steps taken.
 	 */
-	public boolean editMindfulnessExerciseAttempt(MindfulnessExerciseAttempt mindfulnessExerciseAttempt) {
-		// TODO - implement FDMEmployee.editMindfulnessExerciseAttempt
-		throw new UnsupportedOperationException();
+	public boolean editWorkoutExerciseAttempt(WorkoutExerciseAttempt ExerciseAttempt, long newDuration, int newSteps) {
+		//change duration????? how can i change duration ithout changing end time.... or should I make a new object with adjusted end time????
+		if(mindfulnessExerciseAttempt instanceof StepExerciseAttempt){
+			mindfulnessExerciseAttempt.editEntry(newDuration, newSteps);
+		} else{
+			mindfulnessExerciseAttempt.editEntry(newDuration);
+		}
+		
 	}
 
 	/**
 	 * 
-	 * @param workoutExerciseAttempt
+	 * @param ExerciseAttempt the activity to be changed
+	 * @param newDuration  the new time the activity actually took 
 	 */
-	public boolean editWorkoutExercise(WorkoutExerciseAttempt workoutExerciseAttempt) {
-		// TODO - implement FDMEmployee.editWorkoutExercise
-		throw new UnsupportedOperationException();
+	public boolean editMindfulnessExercise(MindfulnessExerciseAttempt ExerciseAttempt, long newDuration) {
+
+		mindfulnessExerciseAttempt.editEntry(newDuration);
 	}
 
 }
