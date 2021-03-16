2275f22ed31df1bbfeb29d5bdf2f68b4d0e18ba9
Oliver Patrick Gannon <o.p.gannon@se19.qmul.ac.uk>
2021-03-16 16:00:52 +0000

    Editing attempt heirarchy

--- src/main/MindfulnessExerciseAttempt.java
+++ src/main/MindfulnessExerciseAttempt.java
@@ -1,15 +1,21 @@
 package main;
 public class MindfulnessExerciseAttempt extends ExerciseAttempt {
 
+
 	MindfulnessExercise mindfulnessExercise;
+
+	public MindfulnessExerciseAttempt(int aattemptNumber, MindfulnessExercise mmindfulnessExercise) {
+		super(aattemptNumber);
+		this.mindfulnessExercise = mmindfulnessExercise;
+	}
+
+	
+
 
 	/**
 	 * 
 	 * @param mmindfulnessExercise
 	 */
-	public void MindfulnessExerciseAttempt(MindfulnessExercise mmindfulnessExercise) {
-		// TODO - implement MindfulnessExerciseAttempt.MindfulnessExerciseAttempt
-		throw new UnsupportedOperationException();
-	}
+	
 
 }
