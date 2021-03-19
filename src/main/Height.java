3b02d3e3c6893fdf6c7558ee28568bbc4b5feacd
Ali <alinajafi1609@gmail.com>
2021-03-19 18:52:38 +0000

    18:52

--- src/main/Height.java
+++ src/main/Height.java
@@ -1,21 +1,18 @@
 package main;
 public class Height extends HealthProperty {
 
-	private ID dataID;
 	private int height;
 
-	public boolean Height(int hheight, LocalDateTime ddateLogged) {
-		// TODO - implement Height.Height
-		throw new UnsupportedOperationException();
+	public Height(int hheight) {
+		height = hheight;
 	}
 
 	public int getHeight() {
 		return this.height;
 	}
 
-	public boolean setHeight(int newHeight) {
-		// TODO - implement Height.setHeight
-		throw new UnsupportedOperationException();
+	public void setHeight(int newHeight) {
+		this.height = newHeight;
 	}
 
 }
