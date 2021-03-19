a81c74218b381069d75ac2123fa4e0de48e3c0d9
Ali <alinajafi1609@gmail.com>
2021-03-19 18:54:46 +0000

    Update Calorie.java

--- src/main/Calorie.java
+++ src/main/Calorie.java
@@ -1,47 +1,20 @@
 package main;
 
-import java.time.LocalDateTime;
 
 public class Calorie extends HealthProperty {
 
 	private int calories;
-	private String food;
 
-	/**
-	 * 
-	 * @param ccalories
-	 * @param ffood
-	 * @param ddateLogged
-	 */
-	public boolean Calorie(int ccalories, String ffood, LocalDateTime ddateLogged) {
-		// TODO - implement Calorie.Calorie
-		throw new UnsupportedOperationException();
+	public Calorie(int ccalories) {
+		calories = ccalories;
 	}
 
 	public int getCalories() {
 		return this.calories;
 	}
 
-	public String getFood() {
-		return this.food;
-	}
-
-	/**
-	 * 
-	 * @param newCalories
-	 */
-	public boolean setCalories(int newCalories) {
-		// TODO - implement Calorie.setCalories
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param newFood
-	 */
-	public boolean setFood(String newFood) {
-		// TODO - implement Calorie.setFood
-		throw new UnsupportedOperationException();
+	public void setCalories(int newCalories) {
+		this.calories = newCalories;
 	}
 
 }
