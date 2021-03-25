da6bd34709ec5cf0c95023137b88f95804c8ac95
Oliver Patrick Gannon <o.p.gannon@se19.qmul.ac.uk>
2021-03-25 14:41:41 +0000

    updating and removing more errors :))))))

--- src/main/GuidedMeditation.java
+++ src/main/GuidedMeditation.java
@@ -1,14 +1,8 @@
 package main;
 public class GuidedMeditation extends MindfulnessExercise {
 
-	/**
-	 * 
-	 * @param eexerciseName
-	 * @param ssoundPath
-	 */
-	public void GuidedMeditation(String eexerciseName, String ssoundPath) {
-		// TODO - implement GuidedMeditation.GuidedMeditation
-		throw new UnsupportedOperationException();
+	public GuidedMeditation(String eexerciseName, String ssoundPath) {
+		super(eexerciseName, ssoundPath);
 	}
 
 }
