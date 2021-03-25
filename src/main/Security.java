401834208422b70a3fa26fa1c7368b9a6ed645a6
Ali <alinajafi1609@gmail.com>
2021-03-25 12:37:40 +0000

    Update Security.java

--- src/main/Security.java
+++ src/main/Security.java
@@ -6,18 +6,18 @@ public class Security {
 	private String password;
 	private Boolean unlocked;
 	private String email;
-	private ArrayList<Integer> codes;
+	private int randomCode;
 
 	public Security(String password, String userName, String email) {
 		this.password = password;
 		this.userName = userName;
 		this.unlocked = false;
 		this.email = email;
-		this.codes = new ArrayList<Integer>();
+		this.randomCode = this.generateForgotPasswordCode();
 	}
 
 	public void login(String userNameORemail, String password) {
-		if((this.userName.equals(userNameORemail) || this.email.equals(userNameORemail)) && this.password.equals(password)){this.unlocked = true;}
+		if((this.userName.equals(userNameORemail) || this.email.equals(userNameORemail)) && this.password.equals(password)) this.unlocked = true;
 	}
 
 	public void changeUserName(String newUserName) {
@@ -33,14 +33,14 @@ public class Security {
 	}
 
 	public int generateForgotPasswordCode() {
-		int min = 100000, max = 999999, codeToSend = min;
-		while(!(codes.contains(codeToSend))){codeToSend = (int)(Math.random() * (max - min + 1) + min);}
-		codes.add(codeToSend);
+		Integer min = 100000, max = 999999, codeToSend = min;
+		codeToSend = (int)(Math.random() * (max - min + 1) + min);
+		this.randomCode = codeToSend;
 		return codeToSend;
 	}
 
 	public boolean forgotPasswordCheckCode(int code) {
-		if (codes.get(codes.size()-1) == code){ return true;}
+		if (randomCode == code) return true;
 		return false;
 	}
 
