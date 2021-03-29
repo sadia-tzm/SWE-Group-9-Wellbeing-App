cf9feb5a015784e6bc786987d7e85ef62fee7799
ijs1506 <i.j.stares@se19.qmul.ac.uk>
2021-03-29 21:09:40 +0100

    testing something

--- wellbeing_app/src/components/CalorieTracker.js
+++ wellbeing_app/src/components/CalorieTracker.js
@@ -1,11 +1,21 @@
 import styled from 'styled-components';
 import React from 'react';
+import '../stylesheets/calorieTracker.css';
 
 export default class CalorieTracker extends React.Component {
 
   render() {
     return (
-      <h1>Calorie Tracker</h1>
+      <div>
+        <h1>Calorie Tracker </h1>
+        <table class="small1">
+                <td class="item"><button>Add Food</button></td>
+                <td class="item"><button>Delete Food</button></td>
+                <td class="item"><button>View Calorie Intake</button></td>
+                <td class="item"><button>View Foods</button></td>
+        </table>
+        </div>
+      
     )
   }
 }
