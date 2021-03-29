e7c0e93c62987aa02736944211fe07e7bcff6a70
Ali <alinajafi1609@gmail.com>
2021-03-29 04:39:51 +0100

    Update WorkoutExcerciseAttempt.java

--- src/main/WorkoutExcerciseAttempt.java
+++ src/main/WorkoutExcerciseAttempt.java
@@ -1,19 +1,14 @@
 package main;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-
 public class WorkoutExcerciseAttempt extends ExerciseAttempt {
 
 	WorkoutExercise workoutExercise;
 	private int caloriesBurnt;
-	private long duration;
 
-	public WorkoutExcerciseAttempt(LocalDateTime dateCompleted, LocalDateTime dateLogged,  WorkoutExercise workoutExercise, int caloriesBurnt, long duration, Instant startTime, Instant endTime, Exercise exercise, int attemptNumber) {
-		super(dateCompleted, dateLogged, duration, attemptNumber, endTime, exercise);
+	public WorkoutExcerciseAttempt( WorkoutExercise workoutExercise, int caloriesBurnt, Exercise exercise, int attemptNumber) {
+		super(attemptNumber, exercise);
 		this.workoutExercise = workoutExercise;
 		this.caloriesBurnt = caloriesBurnt;
-		this.duration = duration;
 	}
 
 	public int getCaloriesBurnt() {
@@ -24,17 +19,7 @@ public class WorkoutExcerciseAttempt extends ExerciseAttempt {
 		return this.workoutExercise;
 	}
 
-	public void editEntry(long newDuration, int newCaloriesBurnt) {
-		this.duration = newDuration;
-		this.caloriesBurnt = newCaloriesBurnt;
-	}
-
 	public void editEntry(int newCaloriesBurnt) {
 		this.caloriesBurnt = newCaloriesBurnt;
 	}
-
-	public void editEntry(long newDuration) {
-		this.duration = newDuration;
-	}
-
 }
