56c06922e13e843f27b5efe2ec72382c4c0f6075
ijs1506 <i.j.stares@se19.qmul.ac.uk>
2021-04-03 16:05:23 +0100

    test

--- wellbeing_app/src/components/HealthInfo.js
+++ wellbeing_app/src/components/HealthInfo.js
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
 import React from 'react';
+import '../stylesheets/HealthInfo.css';
 
 export default class HealthInfo extends React.Component {
 
   render() {
     return (
       <div>
-        <h1>Height and Weight</h1>
-        <h1>BMI</h1>
+        <h1 className="homeHeading">Health Info</h1>
+        
       </div>
     )
   }
