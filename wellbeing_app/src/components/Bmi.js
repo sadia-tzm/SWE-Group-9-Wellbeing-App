77de1ea20f48e1f147aa496ccdefe24f3c1a070b
ijs1506 <i.j.stares@se19.qmul.ac.uk>
2021-04-04 09:55:34 +0100

    HealthInfo Page

---
+++ wellbeing_app/src/components/Bmi.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import React from 'react';
+import '../stylesheets/Bmi.css';
+
+export default class Bmi extends React.Component {
+    constructor(props) {
+    super(props);
+    }
+
+
+
+render() {
+    return (
+        <div>
+            <h1>BMI</h1>
+        </div>
+    )
+}
+}
