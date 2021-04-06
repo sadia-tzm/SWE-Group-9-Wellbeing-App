7e296bdf8f191aace003f41ea21c9fa277845d2c
Syeda Sadia Taznim <s.s.taznim@se19.qmul.ac.uk>
2021-04-06 23:05:55 +0100

    Add Calories page

--- wellbeing_app/src/components/AddCalories.js
+++ wellbeing_app/src/components/AddCalories.js
@@ -1,14 +1,33 @@
 import styled from 'styled-components';
 import React from 'react';
-/*import '../stylesheets/AddCalories.css';*/
+import '../stylesheets/AddCalories.css';
 
 export default class AddCalories extends React.Component {
 
     render() {
         return (
-            <div>
-                <h3>Add Calories</h3>
-            </div>
+            <form className="form4">
+                <h5 className="heading4">Add Calories</h5>
+                <input className="input4"
+                    type='text'
+                    name='food'
+                    placeholder='Food'
+                />
+                <input className="input4"
+                    type='number'
+                    name='weight'
+                    placeholder="Weight"
+                />
+                <input className="input4"
+                    type='number'
+                    name='calories'
+                    placeholder="Calories"
+                />
+                <input className="submit4"
+                    type="submit"
+                    value="Submit"
+                />
+            </form>
         )
     }
 }
