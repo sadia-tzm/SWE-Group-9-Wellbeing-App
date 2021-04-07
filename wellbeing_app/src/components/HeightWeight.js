3844fa5e9221273321b083b6c85f610dcc00a97b
ijs1506 <i.j.stares@se19.qmul.ac.uk>
2021-04-07 11:29:49 +0100

    actually done

--- wellbeing_app/src/components/HeightWeight.js
+++ wellbeing_app/src/components/HeightWeight.js
@@ -7,23 +7,23 @@ export default class HeightWeight extends React.Component {
   render() {
     return (
         <div>
-            <h3 class="pagetext">Title here</h3>
+            <h3 class="pagetext2">Title here</h3>
             <div class="grid-container2">
-                <div class="heading5">Height (cm)</div>
-                <div class="heading5">Weight (Kg)</div>
-                <div class="heading5">Date</div>
-                <div class="text5">190</div>
-                <div class="text5">70</div>
-                <div class="text5">07/04/21</div>
-                <div class="text5">190</div>
-                <div class="text5">70</div>
-                <div class="text5">07/04/21</div>
-                <div class="text5">190</div>
-                <div class="text5">70</div>
-                <div class="text5">07/04/21</div>
-                <div class="text5">190</div>
-                <div class="text5">70</div>
-                <div class="text5">07/04/21</div>
+                <div class="heading6">Height (cm)</div>
+                <div class="heading6">Weight (Kg)</div>
+                <div class="heading6">Date</div>
+                <div class="text6">190</div>
+                <div class="text6">70</div>
+                <div class="text6">07/04/21</div>
+                <div class="text6">190</div>
+                <div class="text6">70</div>
+                <div class="text6">07/04/21</div>
+                <div class="text6">190</div>
+                <div class="text6">70</div>
+                <div class="text6">07/04/21</div>
+                <div class="text6">190</div>
+                <div class="text6">70</div>
+                <div class="text6">07/04/21</div>
             </div>
         </div>
     )
