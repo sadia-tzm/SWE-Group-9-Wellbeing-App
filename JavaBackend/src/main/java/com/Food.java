f39ce781236c036842d0b6083466a2082cfa7b2a
shannon <s.francis@se19.qmul.ac.uk>
2021-04-11 22:32:38 +0100

    finalised food testing

--- JavaBackend/src/main/java/com/Food.java
+++ JavaBackend/src/main/java/com/Food.java
@@ -1,14 +1,18 @@
 package com;
 
+import java.lang.Math.*;
+
 public class Food {
     private String name;
     private int caloriesPer100g;
-    private int weight;
-
-    public Food(String name, int calories, int weight) {
+    private double weight;
+    /**
+    * weight in grams
+    */
+    public Food(String name, int calories, double weight) {
         this.name = name;
         this.weight = weight;
-        this.caloriesPer100g = calories/10/this.weight;
+        this.caloriesPer100g = (int)(((double)calories/this.weight)*100.0);
     }
 
     public String getName(){
@@ -18,8 +22,13 @@ public class Food {
     public int getCalories(){
         return this.caloriesPer100g;
     }
-    
-    public int calculateTotalCalories(int weight){
-        return caloriesPer100g * 10 * weight;
+    /**
+     * calculates the calories for a food given its weight, and using the relative calories per 100grams
+     * @param w
+     * @return
+     */
+    public int calculateTotalCalories(double w){
+
+        return (int) Math.ceil((((double)this.caloriesPer100g/(double)100) * w));
     }
 }
