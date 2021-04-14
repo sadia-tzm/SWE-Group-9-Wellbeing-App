730b8bc7de3a91465ce5b3110edad9be67857f2d
Ali <alinajafi1609@gmail.com>
2021-04-14 16:01:32 +0100

    Update AliTest.java

--- JavaBackend/src/test/java/com/AliTest.java
+++ JavaBackend/src/test/java/com/AliTest.java
@@ -18,56 +18,72 @@ public class AliTest {
     }
 
     @Test
-    @DisplayName("Delete Entry Test")
-    public void testDeleteEntry() {
-        //Weight
-        employee.health.logWeight(120);
-        assertTrue(employee.health.checkEntry(employee.health.getCurrentWeight()));
-        Weight w = employee.health.getCurrentWeight();
-        employee.health.deleteWeightEntry(employee.health.getCurrentWeight());
-        assertFalse(employee.health.checkEntry(w));
-
-        //Height
-        employee.health.logHeight(203);
-        assertTrue(employee.health.checkEntry(employee.health.getCurrentHeight()));
-        Height h = employee.health.getCurrentHeight();
-        employee.health.deleteHeightEntry(employee.health.getCurrentHeight());
-        assertFalse(employee.health.checkEntry(h));
-
-
-        //Calorie
-        employee.health.logCalories(new Food("Apple", 300, 0.3), 0.3);
-        Calorie c = employee.health.getCalorieHistory().get(employee.health.getCalorieHistory().size()-1);
-        assertTrue(employee.health.checkEntry(c));
-        employee.health.deleteCalorieEntry(c);
-        assertFalse(employee.health.checkEntry(c));
+    @DisplayName("Delete Weight Entry Test")
+    public void testDeleteWeightEntry() {
+        employee.getHealthHistory().logWeight(120);
+        assertTrue(employee.getHealthHistory().checkEntry(employee.getHealthHistory().getCurrentWeight()));
+        Weight w = employee.getHealthHistory().getCurrentWeight();
+        employee.getHealthHistory().deleteWeightEntry(employee.getHealthHistory().getCurrentWeight());
+        assertFalse(employee.getHealthHistory().checkEntry(w));
     }
 
     @Test
-    @DisplayName("Edit Entry Test") 
-    public void testEditEntry(){
-        //Weight
-        int w = employee.health.getCurrentWeight().getWeight();
-        employee.health.editWeightEntry(employee.health.getCurrentWeight(), 96);;
-        assertFalse(w==employee.health.getCurrentWeight().getWeight());
+    @DisplayName("Delete Height Entry Test")
 
-        //Height
-        int h = employee.health.getCurrentHeight().getHeight();
-        employee.health.editHeightEntry(employee.health.getCurrentHeight(), 190);
-        assertFalse(h == employee.health.getCurrentHeight().getHeight());
-        
-        //Calorie
-        employee.health.logCalories(new Food("Apple", 300, 0.3), 0.3);
-        int c = employee.health.getLastCalorieEntry().getCalories();
-        employee.health.editCalorieEntry(employee.health.getLastCalorieEntry(), 100);
-        assertFalse(c == employee.health.getLastCalorieEntry().getCalories());
+    public void testDeleteHeightEntry(){
+        employee.getHealthHistory().logHeight(203);
+        assertTrue(employee.getHealthHistory().checkEntry(employee.getHealthHistory().getCurrentHeight()));
+        Height h = employee.getHealthHistory().getCurrentHeight();
+        employee.getHealthHistory().deleteHeightEntry(employee.getHealthHistory().getCurrentHeight());
+        assertFalse(employee.getHealthHistory().checkEntry(h));
+    }
 
-        //Calorie Otherway
-        employee.health.logCalories(new Food("Aple", 300, 0.3), 0.3);
+    
+    @Test
+    @DisplayName("Delete Calorie Entry Test")
+
+    public void testDeleteCalorieEntry(){
+        employee.getHealthHistory().logCalories(new Food("Apple", 300, 0.3), 0.3);
+        Calorie c = employee.getHealthHistory().getCalorieHistory().get(employee.getHealthHistory().getCalorieHistory().size()-1);
+        assertTrue(employee.getHealthHistory().checkEntry(c));
+        employee.getHealthHistory().deleteCalorieEntry(c);
+        assertFalse(employee.getHealthHistory().checkEntry(c));
+    }
+
+    @Test
+    @DisplayName("Edit Weight Entry Test") 
+    public void testWeightEditEntry(){
+        int w = employee.getHealthHistory().getCurrentWeight().getWeight();
+        employee.getHealthHistory().editWeightEntry(employee.getHealthHistory().getCurrentWeight(), 96);;
+        assertFalse(w==employee.getHealthHistory().getCurrentWeight().getWeight());
+    }
+
+    
+    @Test
+    @DisplayName("Edit Height Entry Test") 
+    public void testHeightEditEntry(){
+        int h = employee.getHealthHistory().getCurrentHeight().getHeight();
+        employee.getHealthHistory().editHeightEntry(employee.getHealthHistory().getCurrentHeight(), 190);
+        assertFalse(h == employee.getHealthHistory().getCurrentHeight().getHeight());
+    }
+
+    @Test
+    @DisplayName("Edit Calorie Entry Test") 
+    public void testCalorieEditEntry(){
+        employee.getHealthHistory().logCalories(new Food("Apple", 300, 0.3), 0.3);
+        int c = employee.getHealthHistory().getLastCalorieEntry().getCalories();
+        employee.getHealthHistory().editCalorieEntry(employee.getHealthHistory().getLastCalorieEntry(), 100);
+        assertFalse(c == employee.getHealthHistory().getLastCalorieEntry().getCalories());
+    }
+
+    @Test
+    @DisplayName("Edit Other Calorie Entry Test") 
+    public void testOtherCalorieEditEntry(){
+        employee.getHealthHistory().logCalories(new Food("Aple", 300, 0.3), 0.3);
         Food f = new Food("Apple",300,0.3);
-        String name = employee.health.getLastCalorieEntry().getFood().getName();
-        employee.health.editCalorieEntry(employee.health.getLastCalorieEntry(), 300, f);
-        assertFalse(name.equals(employee.health.getLastCalorieEntry().getFood().getName()));
+        String name = employee.getHealthHistory().getLastCalorieEntry().getFood().getName();
+        employee.getHealthHistory().editCalorieEntry(employee.getHealthHistory().getLastCalorieEntry(), 300, f);
+        assertFalse(name.equals(employee.getHealthHistory().getLastCalorieEntry().getFood().getName()));
     }
 
 }
