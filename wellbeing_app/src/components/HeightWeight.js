0ace19306fdae00e5bf6ffe1cd8864a0152947bb
Syeda Sadia Taznim <s.s.taznim@se19.qmul.ac.uk>
2021-04-15 03:56:09 +0100

    viewing data stored on firebase

--- wellbeing_app/src/components/HeightWeight.js
+++ wellbeing_app/src/components/HeightWeight.js
@@ -11,7 +11,8 @@ export default class HeightWeight extends React.Component {
             weights: [],
             start: false,
             bmis: [],
-            dates: []
+            dates: [],
+            empty: false
         };
     }
 
@@ -29,15 +30,22 @@ export default class HeightWeight extends React.Component {
                     weights: data.weights,
                     bmis: data.bmis,
                     dates: data.dates,
+                    empty: true
                 });
                 console.log("heights:", this.state.heights);
             });
     }
 
+    componentDidMount() {
+        //if(!this.state.empty){
+        this.getHistory();
+        // }
+    }
+
     render() {
         return (
             <div>
-                <h3 class="pagetext2">View your previously recorded health details</h3>
+                {/* <h3 class="pagetext2">View your previously recorded health details</h3>
                 <div class="grid-container2">
                     <div class="heading6">Height (cm)</div>
                     <div class="heading6">Weight (Kg)</div>
@@ -59,8 +67,8 @@ export default class HeightWeight extends React.Component {
                     <div class="text6">70</div>
                     <div class="text6">25</div>
                     <div class="text6">07/04/21</div>
-                </div>
-                <button onClick={this.getHistory}>Get History</button>
+                </div> */}
+                {/* <button onClick={this.getHistory}>Get History</button> */}
                 {/* <h4>
                     {this.state.heights.map((heights) =>
                         <h4>Heights: {heights}</h4>
@@ -81,30 +89,30 @@ export default class HeightWeight extends React.Component {
                         <h4>Dates: {dates}</h4>
                     )}
                 </h4> */}
-                <table>
-                    <tr>
+                <table className={!this.state.empty ? "historyTable" : ""}>
+                    <tr className="historyHeadings">
                         <th>Height</th>
                         <th>Weight</th>
                         <th>BMI</th>
                         <th>Date</th>
                     </tr>
                     <tr>
-                        <td>
+                        <td className="historyTexts">
                             {this.state.heights.map((heights) =>
                                 <h4>{heights}</h4>
                             )}
                         </td>
-                        <td>
+                        <td className="historyTexts">
                             {this.state.weights.map((weights) =>
                                 <h4>{weights}</h4>
                             )}
                         </td>
-                        <td>
+                        <td className="historyTexts">
                             {this.state.bmis.map((bmis) =>
                                 <h4>{bmis}</h4>
                             )}
                         </td>
-                        <td>
+                        <td className="historyTexts">
                             {this.state.dates.map((dates) =>
                                 <h4>{dates}</h4>
                             )}
