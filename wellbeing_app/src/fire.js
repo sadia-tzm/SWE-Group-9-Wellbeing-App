6c78df2f3711f4e88d93339e62ce3c326b233a81
Oliver Patrick Gannon <o.p.gannon@se19.qmul.ac.uk>
2021-04-15 23:20:50 +0100

    fixed keys

--- wellbeing_app/src/fire.js
+++ wellbeing_app/src/fire.js
@@ -1,14 +1,6 @@
 import firebase from 'firebase';
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCE7oOf6ktQwloZHIuV4ywkx-6UC9mQclc",
-    authDomain: "fdm-wellbeing-app.firebaseapp.com",
-    projectId: "fdm-wellbeing-app",
-    storageBucket: "fdm-wellbeing-app.appspot.com",
-    messagingSenderId: "809483850489",
-    appId: "1:809483850489:web:aa311114def50617ad8828",
-    measurementId: "G-R1RYQM91WM"
-};
+//put firebaseConfig var here
 
 const fire = firebase.initializeApp(firebaseConfig);
 
